46fc9e1a4fcd3cd87634967ef2e5aa6ad0eb4daf
Harry Rickards <hrickards@gmail.com>
2012-11-24 21:11:21 +0000

    Fix bugs

--- server.py
+++ server.py
@@ -28,7 +28,7 @@ def question_text(key):
 def answer_texts(key):
     # TODO mp_change, party_change, election_reason, vvt
     if key in ['party', 'region', 'gender']: answers = possible_answers(key)
-    elif key in ['mp_change', 'party_change', 'election_reason', 'vvt']: answers = []
+    elif key in ['mp_change', 'party_change', 'election_reason', 'vvt']: answers = ["YOU PIECE OF SHIT HARRY IMPLEMENT IT"]
     else: answers = map(get_vote_type, possible_answers(key))
     answers.append("Not sure")
     return answers
@@ -49,7 +49,10 @@ def get_votes(key, party): return vdm.find({"PARTY_NAME": party, "DivisionID": k
 def get_vote_type(vote_type): return voterecordvotetype.find_one({'VoteTypeID':vote_type})['VoteType']
 def get_vote_id(vote_type): return voterecordvotetype.find_one({'VoteType':vote_type})['VoteTypeID']
 
-def least_covariance(orig_data, cols_to_skip):
+def least_covariance(orig_data, cols_to_skip, keys_to_skip):
+    modelen = np.bincount(map(len, orig_data)).argmax()
+    orig_data = filter(lambda r: len(r) == modelen, orig_data)
+
     cols_to_skip.extend(['first_name', 'last_name', '_id'])
     cols_to_hash = ['party', 'mp_change', 'party_change', 'election_reason', 'region', 'vvt', 'gender']
 
@@ -73,18 +76,21 @@ def least_covariance(orig_data, cols_to_skip):
     covs = list(covs)
 
     covs = zip(cols, covs)
-    covs = sorted(covs, key = lambda x: x[1], reverse = True)
+    covs = sorted(covs, key = lambda x: x[1], reverse=True)
+    covs = filter(lambda r: not r[0] in keys_to_skip, covs)
     return covs[0][0]
 
 def get_response_for_key(key): return {'question': question_text(key), 'question_id': key, 'answers': answer_texts(key)}
 
 class App(object):
     def index(self, **answers):
+        keys_to_skip = []
         for k in answers.keys():
-            if answers[k] == "Not sure": del answers[k]
+            if answers[k] == "Not sure":
+                keys_to_skip.append(k)
+                del answers[k]
             else: answers[k] = parse_answer(k, answers[k])
         response = {}
-        print answers
         if len(answers) == 0:
             response = get_response_for_key('region')
         else:
@@ -94,7 +100,7 @@ class App(object):
             if cost == 0: response = {"error": "No match found"}
             elif cost < EPSILON: response = {"success": "%s %s" % (mps[0]['first_name'], mps[0]['last_name'])}
             else:
-                next_col = least_covariance(mps, map(lambda r: r[0], answers))
+                next_col = least_covariance(mps, map(lambda r: r[0], answers), keys_to_skip)
                 response = get_response_for_key(next_col)
         return json.dumps(response)
     index.exposed = True
